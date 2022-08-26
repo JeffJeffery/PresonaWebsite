5c880841f349614ee52b8a4e4b6a8915a69075f5
Ben-Ferreira <benjamin@ferreira-Family.com>
2022-08-26 14:13:36 -0500

    spinner kinda working

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -54,19 +54,19 @@ const About = () => {
             <FontAwesomeIcon icon={faAngular} color="#DD0031" />
           </div>
           <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
           </div>
           <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
           </div>
           <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#DD0031" />
+            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
           </div>
           <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
           </div>
           <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
           </div>
         </div>
       </div>
